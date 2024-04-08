0d21dfb9cf71f77f8a7932fa401d486106a82d92
Alexandru Iuga <alex.iuga@gmail.com>
2024-04-08 19:32:50 +0300

    fixed failing tests

--- sources/SvgToXaml.Tests/Conversion/PolylineTests/PointsTests.cs
+++ sources/SvgToXaml.Tests/Conversion/PolylineTests/PointsTests.cs
@@ -22,35 +22,47 @@ namespace DustInTheWind.SvgToXaml.Tests.Conversion.PolylineTests;
 public class PointsTests : SvgFileTestsBase
 {
     [Fact]
-    public void HavingCircleWithRadius0_WhenSvgIsParsed_ThenResultedEllipseHasWidth0()
+    public void HavingPolylineWith1Point_WhenSvgIsParsed_ThenResultedPolylineHas1Point()
     {
-        ConvertSvgFile("points-1.svg", canvas =>
+        ConvertSvgFile("polyline-points-1.svg", canvas =>
         {
             Polyline polyline = canvas.GetElementByIndex<Polyline>(0);
 
             polyline.Points.Should().HaveCount(1);
+            polyline.Points[0].X.Should().Be(435.026);
+            polyline.Points[0].Y.Should().Be(543.2);
         });
     }
 
     [Fact]
-    public void HavingCircleWithRadius50_WhenSvgIsParsed_ThenResultedEllipseHasWidth100()
+    public void HavingPolylineWith2Points_WhenSvgIsParsed_ThenResultedPolylineHas2Points()
     {
-        ConvertSvgFile("points-2.svg", canvas =>
+        ConvertSvgFile("polyline-points-2.svg", canvas =>
         {
             Polyline polyline = canvas.GetElementByIndex<Polyline>(0);
 
             polyline.Points.Should().HaveCount(2);
+            polyline.Points[0].X.Should().Be(435.026);
+            polyline.Points[0].Y.Should().Be(543.2);
+            polyline.Points[1].X.Should().Be(392.102);
+            polyline.Points[1].Y.Should().Be(551.987);
         });
     }
 
     [Fact]
-    public void HavingCircleWithRadiusMinus50_WhenSvgIsParsed_ThenThrows()
+    public void HavingPolylineWith3Points_WhenSvgIsParsed_ThenResultedPolylineHas3Points()
     {
-        ConvertSvgFile("points-3.svg", canvas =>
+        ConvertSvgFile("polyline-points-3.svg", canvas =>
         {
             Polyline polyline = canvas.GetElementByIndex<Polyline>(0);
 
             polyline.Points.Should().HaveCount(3);
+            polyline.Points[0].X.Should().Be(435.026);
+            polyline.Points[0].Y.Should().Be(543.2);
+            polyline.Points[1].X.Should().Be(392.102);
+            polyline.Points[1].Y.Should().Be(551.987);
+            polyline.Points[2].X.Should().Be(391.925);
+            polyline.Points[2].Y.Should().Be(551.987);
         });
     }
 }
