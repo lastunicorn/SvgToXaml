405626c2d0b47e4bbf37990d3714bfc84539dcdb
Alexandru Iuga <alex.iuga@gmail.com>
2024-04-20 00:55:27 +0300

    added tests for style title

---
+++ sources/SvgToXaml.Tests/SvgSerialization/StyleTests/TitleTests.cs
@@ -0,0 +1,44 @@
+﻿// SvgToXaml
+// Copyright (C) 2022-2024 Dust in the Wind
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+using DustInTheWind.SvgToXaml.SvgModel;
+
+namespace DustInTheWind.SvgToXaml.Tests.SvgSerialization.StyleTests;
+
+public class TitleTests : SvgFileTestsBase
+{
+    [Fact]
+    public void HavingTitleNotSpecified_WhenSvgIsParsed_ThenTitleIsNull()
+    {
+        ParseSvgFile("style-title-missing.svg", svg =>
+        {
+            SvgStyleSheet svgStyleSheet = svg.Children[0] as SvgStyleSheet;
+
+            svgStyleSheet.Title.Should().BeNull();
+        });
+    }
+
+    [Fact]
+    public void HavingTitle_WhenSvgIsParsed_ThenTypeHasCorrectValue()
+    {
+        ParseSvgFile("style-title.svg", svg =>
+        {
+            SvgStyleSheet svgStyleSheet = svg.Children[0] as SvgStyleSheet;
+
+            svgStyleSheet.Title.Should().Be("Default Styles");
+        });
+    }
+}
